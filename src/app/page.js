73c9d25a1fd4b9014fcd2c8be8a55931732a40d0
garvvin <yugarvinyu@gmail.com>
2025-04-02 23:13:20 -0700

    customization

--- src/app/page.js
+++ src/app/page.js
@@ -14,14 +14,10 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
+            Our World Tomorrow
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Non profit
           </li>
         </ol>
 
@@ -39,7 +35,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Skbidi
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
@@ -47,7 +43,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Work in progress
           </a>
         </div>
       </main>
