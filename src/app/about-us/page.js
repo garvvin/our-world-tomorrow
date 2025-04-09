2af93efe09ac3323d30fa952a50c99c99b127b1c
garvvin <yugarvinyu@gmail.com>
2025-04-08 23:44:16 -0700

    Updated ui to be more mobile friendly (mostly breakpoint sizing adjustments). Added Niyah to credits. Changed about us w/ mission statement.

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -5,12 +5,15 @@ export default function AboutUs() {
   return (
     <>
         <Navbar />
-        <div className='w-full min-h-80 flex flex-col justify-center place-items-center'>
-          <div className='flex flex-col gap-7 justify-center place-items-center w-4/5 max-w-300'>
-            <h2 className='font-extrabold text-xl'>About Us</h2>
-            <div className='flex flex-col place-items-center gap-5 min-w-[425px] max-w-[650px] w-full'>
-            <h4 className='text-xl text-center'>We are some high school kids from KHS, creating this non-profit to help those in need, seeking for a better world tomorrow.</h4>
-            <h4 className='text-xl text-center underline'>Our world tomorrow.</h4>
+        <div className='w-full min-h-80 flex flex-col justify-center place-items-center mt-20'>
+          <div className='flex flex-col gap-7 justify-center place-items-center min-w-[390px] max-w-[650px] w-4/5 max-w-300'>
+            <h2 className='font-extrabold text-xl'>Mission Statement</h2>
+            <h4 className='text-lg sm:text-xl text-center'>We inspire our youth to take action on social and environmental issues that affect our world for a better tomorrow!</h4>
+
+            <h2 className='font-extrabold text-xl mt-10'>Who are we?</h2>
+            <div className='flex flex-col place-items-center gap-5 w-full'>
+              <h4 className='text-lg sm:text-xl text-center'>We are some high school kids from KHS, creating this non-profit to help those in need, seeking for a better world tomorrow.</h4>
+              <h4 className='text-xl text-center underline'>Our world tomorrow.</h4>
             </div>
           </div>
         </div>
--- src/app/credit/page.js
+++ src/app/credit/page.js
@@ -8,20 +8,27 @@ export default function Credit() {
     <>
         <Navbar />
         <div className='w-full min-h-80 flex flex-col justify-center place-items-center'>
-          <div className='flex flex-col gap-10 justify-center place-items-center w-4/5 max-w-300'>
+          <div className='flex flex-col mb-10 gap-10 justify-center place-items-center w-4/5 max-w-300'>
             <h2 className='font-extrabold text-xl'>Credits</h2>
             <div className='flex gap-10 w-full'>
               <Image src='/images/shayla-mug.png' className='rounded-xl w-[200px] h-[223px] md:w-[240px] md:h-[268px] lg:w-[300px] lg:h-[335px]' alt="mugshot of shayla, one of the founders" width={300} height={400}/>
-              <div className='flex flex-col gap-3 pt-20'>
-                <h3 className='font-bold text-lg'>Founder / Program Coordinator</h3>
-                <h3 className='text-lg'>Shayla Marie Mol</h3>
+              <div className='flex flex-col gap-3 pt-15 md:pt-20'>
+                <h3 className='font-bold text-md md:text-lg'>Founder / Program Coordinator</h3>
+                <h3 className='text-md md:text-lg'>Shayla Mol</h3>
               </div>
             </div>
             <div className='flex flex-row-reverse gap-10 w-full'>
+              <Image src='/images/niyah-mug.png' className='rounded-xl w-[200px] h-[223px] md:w-[240px] md:h-[268px] lg:w-[300px] lg:h-[335px]' alt="mugshot of garvin, one of the founders" width={300} height={400}/>
+              <div className='flex flex-col gap-3 pt-15 md:pt-20 text-right'>
+                <h3 className='font-bold text-md md:text-lg'>Founder / Program Director</h3>
+                <h3 className='text-md md:text-lg'>Niyah Palma</h3>
+              </div>
+            </div>
+            <div className='flex gap-10 w-full'>
               <Image src='/images/garvin-mug.png' className='rounded-xl w-[200px] h-[223px] md:w-[240px] md:h-[268px] lg:w-[300px] lg:h-[335px]' alt="mugshot of garvin, one of the founders" width={300} height={400}/>
-              <div className='flex flex-col gap-3 pt-20'>
-                <h3 className='font-bold text-lg'>Co-Founder / Lead Programmer</h3>
-                <h3 className='text-lg'>Garvin Yu</h3>
+              <div className='flex flex-col gap-3 pt-15 md:pt-20'>
+                <h3 className='font-bold text-md md:text-lg'>Lead Programmer</h3>
+                <h3 className='text-md md:text-lg'>Garvin Yu</h3>
               </div>
             </div>
           </div>
