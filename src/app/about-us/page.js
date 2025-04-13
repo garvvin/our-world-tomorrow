83ac658b61c7c6e1f4cf1c2f4107fe060da3dcf3
garvvin <yugarvinyu@gmail.com>
2025-04-13 10:55:29 -0700

    fixed using ' between html tags, now it works

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -15,9 +15,9 @@ export default function AboutUs() {
               <h2 className='font-extrabold text-xl mt-5'>Mission Statement</h2>
               <h4 className='text-lg sm:text-xl text-center'>We inspire our youth to take action on social and environmental issues that affect our world for a better tomorrow!</h4>
               <h2 className='font-extrabold text-xl mt-10'>Who are we?</h2>
-              <h4 className='text-lg sm:text-xl text-center'><i>Our World Tomorrow</i> is a non-profit organization aiming to help disadvantaged people through community service and raising awareness on issues that affect today's world.</h4>
+              <h4 className='text-lg sm:text-xl text-center'><i>Our World Tomorrow</i> is a non-profit organization aiming to help disadvantaged people through community service and raising awareness on issues that affect today&#39;s world.</h4>
               <h2 className='font-extrabold text-xl mt-10'>Our Goals</h2>
-              <h4 className='text-lg sm:text-xl text-center'>Our current goals are to raise money for places of need in <i>Our World Tomorrow's</i> founding city: Tracy, California. We are also planning on further expanding our influence later on. We also want to educate people on issues that may affect them, their loved ones, and/or their community.</h4>
+              <h4 className='text-lg sm:text-xl text-center'>Our current goals are to raise money for places of need in <i>Our World Tomorrow&#39;s</i> founding city: Tracy, California. We are also planning on further expanding our influence later on. We also want to educate people on issues that may affect them, their loved ones, and/or their community.</h4>
             </div>
             <Image src='/images/capybara.jpg' className='rounded-xl min-w-[200px] w-80% h-80% relative bottom-50' alt="capybara pic" width={400} height={300}/>
           </div>
